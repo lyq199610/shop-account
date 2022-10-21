ff6128db7f0e78ca91930967f78fa49a0c24262c
liyongqiang <15216775796@163.com>
2022-10-21 16:06:47 +0800

    项目初始化

---
+++ src/main/java/com/lyq/shopaccount/ShopAccountApplication.java
@@ -0,0 +1,13 @@
+package com.lyq.shopaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyq/shopaccount/ShopAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyq.shopaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
