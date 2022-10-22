d0f20b9a6caf71a51236d8d834d2b143848b6f64
liyongqiang <15216775796@163.com>
2022-10-22 10:37:55 +0800

    first commit

---
+++ common/src/main/java/com/lyq/Utils.java
@@ -0,0 +1,15 @@
+package com.lyq;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author li
+ */
+public class Utils {
+
+    public void test(){
+        Map<String, String> map = new HashMap<>(0);
+        System.out.println("hello world!");
+    }
+}
--- src/main/java/com/lyq/shopaccount/ShopAccountApplication.java
+++ src/main/java/com/lyq/shopaccount/ShopAccountApplication.java
@@ -3,6 +3,9 @@ package com.lyq.shopaccount;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lyq-yt
+ */
 @SpringBootApplication
 public class ShopAccountApplication {
 
